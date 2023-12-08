c8c37f12a4d40be0b1f564f3c2c6d27e9dbc24b5
Rishabh Gupta <rishotics@gmail.com>
2023-12-08 10:10:39 +0530

    proof

--- merkle/src/merkle.rs
+++ merkle/src/merkle.rs
@@ -30,11 +30,44 @@ impl MerkleTree {
             return
         }
         for i in self.hashes.len()..2*self.hashes.len()-1 {
-            self.hashes.push(self.hashes[k*2] + self.hashes[k*2+1]);
+            self.hashes.push(self.hashes[k*2] + self.hashes[(k*2)+1]);
             k += 1;
         }
 
         println!("hashes {:?}", self.hashes);
+    }
+
+    fn get_proof_of_leafi(&self, i: i32) -> Vec<Vec<i32>> {
+        let mut proof = Vec::<Vec<i32>>::new();
+        
+        println!("i {:?}", (self.hashes.len().ilog2() as f32 + 0.).floor() as i32);
+
+        let mut k = i as usize;
+
+        for j in 0..self.hashes.len()/(self.hashes.len().ilog2() as f32 + 0.).floor() as usize {
+            println!("level {:?}", j);
+            if(k%2 == 0 ){
+                if(k+1 >= self.hashes.len()) {
+                    break;
+                }
+                proof.push(vec![self.hashes[k+1], 0]);
+                k = self.values.len() + (k/2);
+            } else {
+                if(k-1 >= self.hashes.len()) {
+                    break;
+                }
+                proof.push(vec![self.hashes[k-1], 1]);
+                k = self.values.len() + (k/2);
+            }
+        }
+
+        // let mut j = i
+        // while j<self.hashes.len() {
+            
+        // }
+
+        println!("proof {:?}", proof);
+        proof         
     }
 }
 
@@ -46,5 +79,7 @@ pub fn merkle_tree() {
     tree.add(3);
     tree.add(4);
     tree.add(5);
+
+    tree.get_proof_of_leafi(5);
 
 }
